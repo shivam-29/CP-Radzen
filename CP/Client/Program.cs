4002a0c6742b45cb1939a00714c26679f23edaa6
shivam <shivam@southpawstudio.co>
2024-02-05 19:04:32 +0530

    UI/UX

--- CP/Client/Program.cs
+++ CP/Client/Program.cs
@@ -9,5 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddRadzenComponents();
-
+builder.Services.AddSingleton<DialogService>();
 await builder.Build().RunAsync();
